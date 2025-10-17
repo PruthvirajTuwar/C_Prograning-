709bbfc65b1b3732774fbc57b28fc9ec437448af
Pruthviraj Tuwar <tuwarpruthviraj@gmail.com>
2025-10-17 11:57:03 +0530

    Add files via upload

---
+++ ClassWork/Day_02/program22.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+ void Display()
+ {
+    printf("Jay Ganesh....\n");
+    printf("Jay Ganesh....\n");
+    printf("Jay Ganesh....\n");
+    printf("Jay Ganesh....\n");
+
+}  
+
+// It Should be int main 
+void main ()
+    {
+       
+       Display();
+    }
