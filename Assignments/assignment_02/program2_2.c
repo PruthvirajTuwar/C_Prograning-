9beccb591a44f5de9587ff27c41e88631f0e51f6
Pruthviraj Tuwar <tuwarpruthviraj@gmail.com>
2025-11-18 14:22:14 +0530

    Add files via upload

---
+++ Assignments/assignment_02/program2_2.c
@@ -0,0 +1,24 @@
+// Accept one number form user and print that number of * on screen.
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    while (iNo > 0)
+    {
+        printf("*");
+        iNo--;
+    }
+    printf("\n");
+}
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number ");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    return 0;
+}
