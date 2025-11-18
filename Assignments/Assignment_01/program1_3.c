ab3c0ba01ada5b3ce659abbc85bcd297109bd572
Pruthviraj Tuwar <tuwarpruthviraj@gmail.com>
2025-11-18 14:20:38 +0530

    Add files via upload

---
+++ Assignments/Assignment_01/program1_3.c
@@ -0,0 +1,33 @@
+// Program to print 5 to 1 number on screen.
+
+#include<stdio.h>
+
+void Display()
+{
+    int i = 0;
+    
+    while ((i <= 5))
+    {
+        printf("%d\n", i);
+        i ++;
+    }
+    
+}
+int main()
+{
+    
+    Display();
+    
+    return 0 ;
+}
+
+////////////////////////////////////////
+//
+// 0
+// 1
+// 2
+// 3
+// 4
+// 5
+//
+////////////////////////////////////////
